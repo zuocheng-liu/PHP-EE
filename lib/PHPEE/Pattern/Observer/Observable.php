8b3caf447bd97ca4b6a011f347a024077e94c310
Zuocheng Liu <zuocheng.liu@gmail.com>
2015-04-07 13:59:38 +0800

    add pattern

---
+++ lib/PHPEE/Pattern/Observer/Observable.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuzuocheng
+ * Date: 2015/4/3
+ * Time: 18:36
+ */
+
+namespace PHPEE\Pattern\Observer;
+
+class Observable {
+
+}
---
+++ lib/PHPEE/Pattern/Observer/Observer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuzuocheng
+ * Date: 2015/4/3
+ * Time: 18:35
+ */
+namespace PHPEE\Pattern\Observer;
+interface Observer {
+
+}
